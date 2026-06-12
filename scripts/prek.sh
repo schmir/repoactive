1d88cf068da4cb9181c8f86df70663e51c36670a
Ralf Schmitt <ralf@systemexit.de>
2026-06-12 22:08:45 +0200

    prek.sh: drop GIT_DIR hack, skip when not a git repo
    The git head could be very much different from the HEAD in a worspace repo.
    So, it's better to fail or do nothing, instead of giving wrong results.

--- scripts/prek.sh
+++ scripts/prek.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# In a jj workspace the working directory has no .git, so prek's internal "get git root" command
-# exits with "fatal: not a git repository". Point GIT_DIR at the main repo's .git so prek works
-# from the workspace.
-if [ -f .jj/repo ]; then
-    export GIT_DIR="$(cat .jj/repo)/../../.git"
+if [ ! -e .git ]; then
+    printf >&2 "not a git repo: cannot run prek\n"
+    exit 0
+
 fi
 
 if command -v prek >/dev/null 2>&1; then
